b84383f68adbcaf04429f3919911ddb7c9b08feb
selmanahmad57@gmail.com <selmanahmad57@gmail.com>
2025-08-27 02:29:28 +0200

    Replace HeaderTemp with Header component and update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import SeoForm from '../components/SeoForm';
-import SeoDashboard from '../components/SeoDashboard';
 
 export default function HomePage() {
   return (
     <>
       <Header />
-      <main className="min-h-screen bg-indigo-100 py-10 space-y-10 px-4">
+      <main>
         <SeoForm />
-        <SeoDashboard />
       </main>
       <Footer />
     </>
